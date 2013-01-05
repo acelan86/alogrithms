e4b7270f2ac132863e64b291551906b92fbb12bb
acelan86 <79480491@qq.com>
2013-01-05 17:14:38 +0800

    增加堆排序

--- sort/heapSort.js
+++ sort/heapSort.js
@@ -1,20 +1,40 @@
-function getParentIndex(i) {
+function Heap(source, heapSize) {
+    this.heapSize = heapSize || source.length;
+    this.length = source.length;
+    this.data = source;
+}
+
+Heap.getParentIndex = function (i) {
     return i >> 1; //Math.floor(i / 2);
-}
-function getLeftIndex(i) {
+};
+Heap.getLeftIndex = function (i) {
     return i << 1;
-}
-function getRightIndex(i) {
+};
+Heap.getRightIndex = function (i) {
     return i << 1 + 1;
 }
-/**
- * 使以i为根的子树成为最大堆
- * @param  {[type]} source [description]
- * @param  {[type]} i      [description]
- * @return {[type]}        [description]
- */
-function maxHeapify(source, i) {
+Heap.maxHeapify = function (heap, i) {
+    var source = heap.data,
+        heapSize = heap.getHeapSize(),
+        l = Heap.getLeftIndex(i),
+        r = Heap.getRightIndex(i),
+        largest,
+        key;
 
+    if (l <= heapSize && source[l] > source[i]) {
+        largest = l;
+    } else {
+        largest = i;
+    }
+    if (r <= heapSize && source[r] > source[i]) {
+        largest = r;
+    }
+    if (largest !== i) {
+        key = source[i];
+        source[i] = source[largest];
+        source[largest] = key;
+        Heap.maxHeapify(source, largest);
+    }
 }
 /**
  * 创建一个最大堆
@@ -22,10 +42,25 @@ function maxHeapify(source, i) {
  * @return {[type]}        [description]
  */
 function buildMaxHeap(source) {
+    var heap = new Heap(souce),
+        souce = heap.data;
 
+    for (i = heap.length >> 1; i >= 0; i--) {
+        Heap.maxHeapify(heap, i);
+    }
+    return heap;
 }
 
 
 function heapSort(source) {
+    var heap = buildMaxHeap(source),
+        source = heap.data,
+        i;
 
+    for (i = heap.getHeapLength(); i >= 1; i--) {
+        key = source[0];
+        source[0] = source[i];
+        heap.heapSize--;
+        Heap.maxHeapify(heap, 1);
+    }
 }

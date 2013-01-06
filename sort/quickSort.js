5eee02ac097be365ec29069062500bed39601f76
acelan86 <79480491@qq.com>
2013-01-06 14:50:53 +0800

    增加快速排序

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -1,27 +1,27 @@
 function partition(source, start, end) {
-	var x = source[end],
-		i = start - 1,
-		j,
-		key;
-	for (j = start; j <= end - 1; j++) {
-		if (source[j] <= x) {
-			i++;
-			key = source[i];
-			source[i] = source[j];
-			source[j] = key;
-		}
-	}
-	key = source[i + 1];
-	source[i + 1] = source[end];
-	source[end] = key;
-	return i + 1;
+    var x = source[end], //选中轴
+        i = start - 1,
+        j,
+        key;
+    for (j = start; j <= end - 1; j++) {
+        if (source[j] <= x) {
+            i++;
+            key = source[i];
+            source[i] = source[j];
+            source[j] = key;
+        }
+    }
+    key = source[i + 1];
+    source[i + 1] = source[end];
+    source[end] = key;
+    return i + 1;
 }
 
 function quickSort(source, start, end) {
-	var middle;
-	if (start < end) {
-		middle = partition(source, start, end);
-		quickSort(source, start, middle - 1);
-		quickSort(souce, middle + 1, end);
-	}
+    var middle;
+    if (start < end) {
+        middle = partition(source, start, end);
+        quickSort(source, start, middle - 1);
+        quickSort(souce, middle + 1, end);
+    }
 }
